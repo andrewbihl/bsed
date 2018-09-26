43671442fc0bd43cfb343fc9760ea7c58fdb5141
Andrew Bihl <andrewbihlva@gmail.com>
2018-09-26 00:02:21 -0700

    Move config folder into project dir

--- bted/definitions.py
+++ bted/definitions.py
@@ -1,6 +1,6 @@
 from os import path
 
-ROOT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+ROOT_DIR = path.dirname(path.abspath(__file__))
 CONFIG_DIR = path.join(ROOT_DIR, 'config')
 COMMAND_TRANSLATIONS_FILE = path.join(CONFIG_DIR, 'command_translations.json')
 COMMAND_TOKEN_TREE = path.join(CONFIG_DIR, 'command_token_tree.json')
