3a65c00d2cd14114f98f78594d55d248f47af591
Andrew Bihl <andrewbihlva@gmail.com>
2018-09-26 11:15:44 -0700

    Extract flags. Also, remove merge_quoted_phrases... it turns out the cli does this automatically

--- bted/arg_process.py
+++ bted/arg_process.py
@@ -1,20 +1,35 @@
+#
+# def merge_quoted_phrases(cmd_args):
+#     """ Merge literal (quote-wrapped) inputs """
+#     res = []
+#     start = -1
+#     for i in range(len(cmd_args)):
+#         arg = cmd_args[i]
+#         if start >= 0:
+#             if arg[-1] == '\"':
+#                 new_arg = ' '.join(cmd_args[start:i+1])
+#                 res.append(new_arg)
+#                 # cmd_args = cmd_args[:start] + [new_arg] + cmd_args[i+1:]
+#                 start = -1
+#                 continue
+#         elif arg[0] == '\"':
+#                 start = i
+#         else:
+#             res.append(arg)
+#     return cmd_args
 
-def merge_quoted_phrases(cmd_args):
-    """ Merge literal (quote-wrapped) inputs """
-    start = -1
+
+def extract_flags(cmd_args):
+    flags = []
+    res = []
     for i in range(len(cmd_args)):
         arg = cmd_args[i]
-        if start >= 0:
-            if arg[-1] == '\"':
-                new_arg = ' '.join(cmd_args[start:i+1])
-                cmd_args = cmd_args[:start] + [new_arg] + cmd_args[i+1:]
-                start = -1
-                continue
+        if arg[0] == '-':
+            flags.append(arg)
         else:
-            if arg[0] == '\"':
-                start = i
-    return cmd_args
+            res.append(arg)
+    return res, flags
 
 
-def process_args(args: [str]) -> [str]:
-    return merge_quoted_phrases(args)
+def process_args(args: [str]) -> ([str], [str]):
+    return extract_flags(args)
