cc4f9f4c76b38cba9057eb87e81ff28c96c3f0ad
Andrew Bihl <andrewbihlva@gmail.com>
2018-09-25 23:16:20 -0700

    Increment val

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bted",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrew Bihl",
     author_email="andrewbihlva@gmail.com",
     description="A simple syntax for text editing.",
