7b1a3137de32e44d117e053310d3234d7a4fbf9f
Andrew Bihl <andrewbihlva@gmail.com>
2018-09-25 22:35:40 -0700

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bted",
+    version="0.0.1",
+    author="Andrew Bihl",
+    author_email="andrewbihlva@gmail.com",
+    description="A simple syntax for text editing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrewbihl/bted",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
