feb42c3dd15608bee7d393598cce5330f4e5da85
andrewbihl <andrewbihlva@gmail.com>
2018-10-20 13:57:52 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bted",
-    version="0.1.5",
+    version="0.1.6",
     author="Andrew Bihl",
     author_email="andrewbihlva@gmail.com",
     description="A simple syntax for text editing.",
