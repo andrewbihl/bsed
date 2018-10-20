42caa832c8f1afb85df5c2fa5db6520d32bc009d
andrewbihl <andrewbihlva@gmail.com>
2018-10-20 14:27:40 -0700

    Shorten test file names. Add test file for select line ranges

--- tests/test_select.py
+++ tests/test_select.py
@@ -43,3 +43,7 @@ class TestSelect(unittest.TestCase):
     def test_select_lines_ending_with_word(self):
         func_name = inspect.stack()[0].function
         self.perform_test_from_key(func_name)
+
+    def test_select_lines_m_to_n(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
