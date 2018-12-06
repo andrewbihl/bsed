fe10497b2352f82055456d67d8cae7b4085990a4
andrewbihl <andrewbihlva@gmail.com>
2018-12-06 01:26:25 -0800

    Add unit tests for upcoming 'on lines...' feature

---
+++ tests/test_on_lines_containing.py
@@ -0,0 +1,52 @@
+import unittest
+from os import path
+import json
+import inspect
+
+from .context import bted
+import bted.definitions as definitions
+from bted import interpreter
+
+append_tests = path.join(definitions.TESTS_DIR, 'test_on_lines_filter.json')
+
+
+class TestOnLinesContaining(unittest.TestCase):
+
+    def setUp(self):
+        command_tree_fp = definitions.COMMAND_TOKEN_TREE
+        translations_fp = definitions.COMMAND_TRANSLATIONS_FILE
+        self.interpreter = interpreter.Interpreter(command_tree_fp, translations_fp)
+        with open(append_tests, 'r') as fin:
+            self.tests = json.load(fin)
+
+    def perform_test(self, command: [str], input_file: str, expected_result_file: str):
+        with open(expected_result_file, 'r') as fin:
+            expected = fin.read()
+        cmd, flags = self.interpreter.build_command(command, input_file)
+        res = self.interpreter.execute_command(cmd, flags, return_output=True)
+        self.assertEqual(expected, res)
+
+    def perform_test_from_key(self, key: str):
+        tests = self.tests[key]
+        for t in tests:
+            self.perform_test(t["command"], path.join(definitions.TEST_FILES_DIR, t["input"]), path.join(definitions.TEST_FILES_DIR, t["expected"]))
+
+    def test_on_lines_delete(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
+
+    def test_on_lines_replace(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
+
+    def test_on_lines_prepend(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
+
+    def test_on_lines_append(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
+
+    def test_on_lines_wrap(self):
+        func_name = inspect.stack()[0].function
+        self.perform_test_from_key(func_name)
