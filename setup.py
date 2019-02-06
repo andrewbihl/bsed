08f25bd7f5562a52a939f2ea35cefa82ad3110b8
andrewbihl <andrewbihlva@gmail.com>
2019-02-05 23:22:50 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bsed",
-    version="0.1.8",
+    version="0.1.9",
     author="Andrew Bihl",
     author_email="andrewbihlva@gmail.com",
     description="Simple english syntax for stream editing.",
